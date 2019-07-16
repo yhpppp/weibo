ef1e59580a3630330d08dd2a461f8ad82e3d5692
yhpp <yhpp1995@gmail.com>
2019-07-16 17:04:06 +0800

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>@yield('title','default title') - ANG</title>
+</head>
+
+<body>
+  <div class="container">
+    @yield('content')
+  </div>
+</body>
+
+</html>
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','帮助中心')
+@section('content')
+<h1>help page</h1>
+@stop

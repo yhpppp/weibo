16a8050c030a86f39cb3fe883c66b4dba567b032
yhpp <yhpp1995@gmail.com>
2019-07-22 10:30:46 +0800

    抽离公共头部与尾部成局部视图

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,23 +1,17 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>@yield('title', 'Weibo App') - AMG</title>
-    <link rel="stylesheet" href="{{mix('css/app.css')}}">
-  </head>
-  <body>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container">
-        <a class="navbar-brand" href="/">Weibo App</a>
-        <ul class="navbar-nav justify-content-end">
-          <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
-          <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
-        </ul>
-      </div>
-    </nav>
+<head>
+  <title>@yield('title', 'Weibo App') - AMG</title>
+  <link rel="stylesheet" href="{{mix('css/app.css')}}">
+</head>
 
-    <div class="container">
-      @yield('content')
-    </div>
-  </body>
+<body>
+  @include('layouts._header')
+  <div class="container">
+    @yield('content')
+    @include('layouts._footer')
+  </div>
+</body>
+
 </html>

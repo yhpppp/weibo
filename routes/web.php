f27c52beb96bd5b127304c5c4bec1a94f23422a0
vagrant <vagrant@homestead>
2019-08-05 08:40:59 +0000

    发布微博

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,6 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 // 执行密码更新操作
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+// 微博的创建和删除操作。
+Route::resource('statuses','StatusesController',['only'=>['store','destroy']]);

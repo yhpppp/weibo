579412c51bad38b5a449d8d9413ab69c31af3cab
vagrant <vagrant@homestead>
2019-08-13 02:39:42 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -66,4 +66,44 @@ class User extends Authenticatable
     {
         return $this->statuses()->orderBy('created_at', 'desc');
     }
+
+
+    // 获取粉丝关系列表
+    public function followers()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+    }
+
+    // 获取用户关注人列表
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+    }
+
+    // 关注 
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    // 取消关注
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    // 判断是否关注
+    // 判断用户 B 是否包含在用户 A 的关注人列表上
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
+    
+    
 }
